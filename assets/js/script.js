014bb70ffc87b397382d75ef69de44cf0541beb9
Hendras Laksono <hexa.official.cs@gmail.com>
2025-10-18 13:07:55 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,26 +10,22 @@ gtag('config', 'G-V1JWDFFYK2');
    Google Translate (stabil)
    ====================================================== */
 function googleTranslateElementInit() {
-  new google.translate.TranslateElement({ pageLanguage: 'id' }, 'google_translate_element');
+  new google.translate.TranslateElement({ pageLanguage: "id" }, "google_translate_element");
 }
 
 function doGTranslate(el) {
-  if (el.value === '') return;
-  var lang = el.value.split('|')[1];
-  if (lang === 'id') {
-    document.cookie = 'googtrans=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-    document.cookie = 'googtrans=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=' + window.location.hostname + ';';
-    location.href = "https://link.laksanacraft.my.id/";
+  if (el.value === "") return;
+  var langPair = el.value.split("|");
+  var lang = langPair[1];
+  if (lang === "id") {
+    document.cookie = "googtrans=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "googtrans=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=" + window.location.hostname + ";";
+    window.location.href = window.location.origin + window.location.pathname;
     return;
   }
-  var select = document.querySelector('.goog-te-combo');
+  var select = document.querySelector(".goog-te-combo");
   if (select) {
     select.value = lang;
-    select.dispatchEvent(new Event('change'));
+    select.dispatchEvent(new Event("change"));
   }
 }
-
-/* Muat script translate Google secara dinamis */
-const gtScript = document.createElement('script');
-gtScript.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
-document.head.appendChild(gtScript);
