1342a77e5eaf60147ba90e59cd34aaad42011493
Hendras Laksono <hexa.official.cs@gmail.com>
2025-10-19 12:54:47 +0700

    Update script.js

--- page/search/script.js
+++ page/search/script.js
@@ -224,25 +224,3 @@ loadAllProducts()
     document.getElementById("results").innerHTML =
       "<p style='color:red;'>⚠️ Gagal memuat data produk.</p>";
   });
-
-// --- Google Translate Support ---
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({ pageLanguage: "id" }, "google_translate_element");
-}
-
-function doGTranslate(el) {
-  if (el.value === "") return;
-  var langPair = el.value.split("|");
-  var lang = langPair[1];
-  if (lang === "id") {
-    document.cookie = "googtrans=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    document.cookie = "googtrans=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=" + window.location.hostname + ";";
-    window.location.href = window.location.origin + window.location.pathname;
-    return;
-  }
-  var select = document.querySelector(".goog-te-combo");
-  if (select) {
-    select.value = lang;
-    select.dispatchEvent(new Event("change"));
-  }
-}
