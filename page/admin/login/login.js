1cdc5b032b427c5f7c399b28eddf02546ef6aec7
Hendras Laksono <hexa.official.cs@gmail.com>
2025-11-19 05:39:09 +0700

    Create login.js

---
+++ page/admin/login/login.js
@@ -0,0 +1,23 @@
+const ADMIN_USER = "admin@laksanacraft";
+const ADMIN_PASS = "laksanacraft-admin";
+
+// Proses login
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const user = document.getElementById("loginUser").value.trim();
+    const pass = document.getElementById("loginPass").value.trim();
+
+    if (user === ADMIN_USER && pass === ADMIN_PASS) {
+        localStorage.setItem("adminLogin", "true");
+        window.location.href = "index.html"; // Redirect ke admin dashboard
+    } else {
+        alert("Login gagal! Username atau password salah.");
+    }
+});
+
+// Tampilkan/Sembunyikan password
+function togglePassword() {
+    const pass = document.getElementById("loginPass");
+    pass.type = pass.type === "password" ? "text" : "password";
+}
