0227aca760ae78c985e9cecdc8b96fc6ddd52e32
Hendras Laksono <hexa.official.cs@gmail.com>
2025-11-22 02:49:10 +0700

    Update script.js

--- page/links/script.js
+++ page/links/script.js
@@ -221,8 +221,8 @@ document.head.appendChild(themeLink);
 // Muat tema tersimpan
 const savedTheme = localStorage.getItem("theme") || "base";
 themeLink.href = savedTheme === "base" 
-    ? "../assets/style/style.css" 
-    : `../assets/style/themes/${savedTheme}.css`;
+    ? "../../assets/style/style.css" 
+    : `../../assets/style/themes/${savedTheme}.css`;
 themeSelector.value = savedTheme;
 
 // Ganti tema saat dipilih
@@ -230,7 +230,7 @@ themeSelector.addEventListener("change", () => {
   const val = themeSelector.value;
   if (!val) return;
   themeLink.href = val === "base" 
-      ? "../assets/style/style.css" 
-      : `../assets/style/themes/${val}.css`;
+      ? "../../assets/style/style.css" 
+      : `../../assets/style/themes/${val}.css`;
   localStorage.setItem("theme", val);
 });
